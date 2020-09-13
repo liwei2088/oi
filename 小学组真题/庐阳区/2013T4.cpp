dae64f8228a388167ed6750f1ca8ddf71eb3f91a
liwei2088 <liwei2088@sohu.com>
2020-09-14 07:28:20 +0800

    2020-9-14

--- 小学组真题/庐阳区/2013T4.cpp
+++ 小学组真题/庐阳区/2013T4.cpp
@@ -4,7 +4,8 @@ int n, f[1005];
 int main() {
     cin >> n;
     f[1] = 1, f[2] = 2;
-    for (int i = 3; i <= n; i++) f[i] = f[i - 1] + f[i - 2];
+    for (int i = 3; i <= n; i++) 
+        f[i] = f[i - 1] + f[i - 2];
     cout << f[n];
     return 0;
 }
