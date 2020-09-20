36b3d567abfaf9ae2404d901ff7b0fc2c8e67b1e
liwei2088 <liwei2088@sohu.com>
2020-09-21 06:49:10 +0800

    2020-9-21

---
+++ leetcode/97ebook/01初级算法/26.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * 双指针算法
+ */
+class Solution {
+   public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+};
+
+int main() { return 0; }
