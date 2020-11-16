7de67dca959e61080e7e6d5fed7b9179d1a8037e
liwei2088 <liwei2088@sohu.com>
2020-11-16 14:32:30 +0800

    2020-11-16

---
+++ 小学组真题/合肥市/2020T1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long a1, a2, b1, b2;
+    cin >> a1 >> a2;
+    cin >> b1 >> b2;
+    cout << a1 * b1 + a2 * b2;
+    return 0;
+}
